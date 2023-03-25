2d4070652390074a86b89260d5578ad17d06e241
nadadd <104659330+nadadd@users.noreply.github.com>
2023-03-25 14:29:11 +0100

    pas encore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ListConsultantComponent } from './list-consultant/list-consultant.component';
 import { AddConsultantComponent } from './add-consultant/add-consultant.component';
 import { EditConsultantComponent } from './edit-consultant/edit-consultant.component';
+import { PopupAddComponent } from './popup-add/popup-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListConsultantComponent,
     AddConsultantComponent,
-    EditConsultantComponent
+    EditConsultantComponent,
+    PopupAddComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/popup-add/popup-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-add',
+  templateUrl: './popup-add.component.html',
+  styleUrls: ['./popup-add.component.css']
+})
+export class PopupAddComponent {
+
+}
